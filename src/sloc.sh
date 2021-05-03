86b09aded406e4d3c9b79c9fdc62db41471cb352
BenniTec <benni@stuerz.xyz>
2021-05-03 20:48:52 +0200

    Improved sloc

--- src/sloc.sh
+++ src/sloc.sh
@@ -8,4 +8,4 @@ filter() {
 
 [ $# -lt 1 ] && echo "Usage: $(basename "$0") <directory...>" && exit 1
 
-find "$@" -type f | filter | xargs wc -l | tail -n1 | awk '{print $1}'
+find "$@" -type f | grep -v '^\./\.[^$]*$' | filter | xargs grep -v '^\s*\(//[^$]*\)\?\s*$' | wc -l | tail -n1 | awk '{print $1}'
